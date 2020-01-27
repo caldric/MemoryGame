0aa1978a37adf64d6d6374c9973d27eed1ef6625
Claude Aldric <cvaldric@gmail.com>
2020-01-27 00:20:18 -0500

    Implement functions per DRY principle

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,36 @@
 let cards = ['queen', 'queen', 'king', 'king'];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
+function addCard(cardId) {
+  console.log(`User flipped ${cards[cardId]}`);
+  cardsInPlay.push(cardId);
+}
 
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
+function isMoveValid(cardId) {
+  if (cardsInPlay.length === 1 && cardsInPlay[0] === cardId) {
+    return false;
+  }
+  return true;
+}
 
-console.log('User flipped ' + cardOne);
-console.log('User flipped ' + cardTwo);
-
-
-if (cardsInPlay.length === 2) {
-  if (cards[0] === cards[2]) {
-    alert('You found a match!');
+function checkForMatch() {
+  if (cards[cardsInPlay[0]] === cards[cardsInPlay[1]]) {
+    console.log("You've found a match!");
   } else {
-    alert('Sorry, try again.');
+    console.log("Sorry, try again.");
+  }
+}
+
+function flipCard(cardId) {
+  if (cardsInPlay.length === 0) {
+    addCard(cardId);
+  } else if (cardsInPlay.length === 1) {
+    if (isMoveValid(cardId)) {
+      addCard(cardId);
+      checkForMatch();
+      cardsInPlay = [];
+    } else {
+      console.log("Invalid move. Please try again.");
+    }
   }
 }
