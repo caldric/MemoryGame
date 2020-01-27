ef04f39cf978b622232728b5ef717380c6c9b3fd
Claude Aldric <cvaldric@gmail.com>
2020-01-27 00:45:00 -0500

    Refactor and add comments

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,12 +1,14 @@
-let cards = ['queen', 'queen', 'king', 'king'];
+let cardNames = ['queen', 'queen', 'king', 'king'];
 let cardsInPlay = [];
 
 function addCard(cardId) {
-  console.log(`User flipped ${cards[cardId]}`);
+  // Add card to the array of selected cards
+  console.log(`User flipped ${cardNames[cardId]}`);
   cardsInPlay.push(cardId);
 }
 
 function isMoveValid(cardId) {
+  // Check whether the next move is valid
   if (cardsInPlay.length === 1 && cardsInPlay[0] === cardId) {
     return false;
   }
@@ -14,7 +16,8 @@ function isMoveValid(cardId) {
 }
 
 function checkForMatch() {
-  if (cards[cardsInPlay[0]] === cards[cardsInPlay[1]]) {
+  // Check if first card matches second card
+  if (cardNames[cardsInPlay[0]] === cardNames[cardsInPlay[1]]) {
     console.log("You've found a match!");
   } else {
     console.log("Sorry, try again.");
@@ -22,15 +25,25 @@ function checkForMatch() {
 }
 
 function flipCard(cardId) {
-  if (cardsInPlay.length === 0) {
+  // Main function that deals with user action
+  if (isMoveValid(cardId)) {
     addCard(cardId);
-  } else if (cardsInPlay.length === 1) {
-    if (isMoveValid(cardId)) {
-      addCard(cardId);
-      checkForMatch();
-      cardsInPlay = [];
-    } else {
-      console.log("Invalid move. Please try again.");
-    }
+  } else {
+    console.log("Invalid move. Please try again.");
+  }
+
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+    cardsInPlay = [];
   }
 }
+
+flipCard(0);
+flipCard(1);
+
+flipCard(0);
+flipCard(2);
+
+flipCard(0);
+flipCard(0);
+flipCard(1);
